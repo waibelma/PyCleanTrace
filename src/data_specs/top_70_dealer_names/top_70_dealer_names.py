86de09a56141644ba85afe668822e8ad5deed3c5
Martin Waibel <martin.waibel@phdstudent.hhs.se>
2022-09-27 17:03:51 +0200

    Add the data specifications

---
+++ src/data_specs/top_70_dealer_names/top_70_dealer_names.py
@@ -0,0 +1,28 @@
+"""Map identities of the Top-70 dealers to more easily comparable names in order to compare active dealers across
+   samples.
+
+"""
+
+remap_dealer_names={
+    '3c8f47d15c0f4649dd34e0e3db66192e5cfbb71a' : 'A', 'ba05b87ec6941d3d9e58fda86193b9d192a52224' : 'B',
+    'c2ec3beda1538e8378ad17e4cbe7966d5f1a9dd6' : 'C', 'b8d10cee019b044d3a396ad56871c3f1e2c5c6b7' : 'D',
+    '69bc4c1f5298e5a8628f0cb79192315ec36aedf2' : 'E', '3a480c1bac4fc53fea8b192a2d19d0559c0d779c' : 'F',
+    '8af09d74f5eac6f5ad9480a2fa02a8c49cc48d5a' : 'G', 'b55e95286b96b38144c340133f80fd5c12fa01c9' : 'H',
+    '0916361b0528a32736f9ce86a6d85a02f4f77ca0' : 'I', 'cf6930c3eb1f429825e61fb1de205927f8820725' : 'J',
+    '78b8bf78a3a5c2cc09d0c2daaad4d23a7b82f935' : 'K', 'a64eb233b06a7e24967451759467a75ff4d06e83' : 'L',
+    '90c341b3c8d78f1c15c079253cbd11d27bb60ab9' : 'M', '7d9adcb9ee27b273e090d8383710cab8e9f48998' : 'N',
+    'fb94e37f4800b755d3903629d4a082751dbf5db7' : 'O', 'aa580f3c45ef85a2c2c4c1618de7d26341de6268' : 'P',
+    '8956044faafa0de2d844dc12522e03555135180e' : 'Q', 'a992080fbb477843af34104247bfa3bfef31630d' : 'R',
+    'fadde9b5b102b5820bef3026e986ac10e13c3855' : 'S', '1a6e90a5d5ecbf18ee4ca7dc382c4bfa77170cd2' : 'T',
+    '3251b2d98f6977e230dfe65488ead9c5cc0ff211' : 'U', '36232cfb7816f6a3fec3734cbebbdd1dfc4adeac' : 'V',
+    '38e96bda98db514f0768307e2573dbc6ecf98c1e' : 'V', '3dbe7479832f08aee2edb01c577848e9c8a155f6' : 'W',
+    '43e236e11f1acddccdc9ac14425b9bb00ba9b884' : 'X', '67333637c31439b7a6b62d82e32b4d6de6a8909b' : 'Y',
+    '68feae461bfd58a6972ff3b1afdb9530138a7f5c' : 'Y',
+    '770f747572ab49afd06be2f2405debc83d7cd01c' : 'ZA', '8442ee73b16857b09a20b49a318dea49f6ac064d' : 'ZB',
+    '86fbd50db2cbebda812d684aad49f423c12ff3d6' : 'ZC', 'c03205f518416076b480b87e85a8703774e537b2' : 'ZD',
+    'ce16e366b2557869c61130102f50d71c4c98275b' : 'ZE', 'd0b47413a36d044cfcee03e5a17de22b39c08802' : 'ZF',
+    'd26f34ad87880e7b613b8d7c92e3508f415382b0' : 'ZG', 'd80eefbc7644c779a4dc75ce850cdc67af061779' : 'ZH'
+}
+
+
+
