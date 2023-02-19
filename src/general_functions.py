0cc60ff3dc8fe280f1c829f8c29da0cb972292a8
Martin Waibel <martin.waibel@phdstudent.hhs.se>
2023-02-19 14:43:02 +0100

    CHange typo

--- src/general_functions.py
+++ src/general_functions.py
@@ -1,10 +1,9 @@
 import os.path
 from sys import platform
 
-
 def build_folders(path_in):
-     """Check if an exisiting file path exists. If not create the necessry folder such that the 
-     file path existis.
+    """Check if an exisiting file path exists. If not create the necessry folder such that the 
+    file path existis.
 
     Args:
     --------
@@ -13,11 +12,12 @@ def build_folders(path_in):
     Returns:
     --------
     Generates the relevant file path
-
     """
 
+    print("Hi")
     if not os.path.exists(path_in):
-        if ((platform=="win32") | (platform=="win64")) :
+        print("Hi")
+        if ((platform=="win32") | (platform=="win64")):
             os.mkdir(path_in)
             #os.system("chmod 777 " + path_in)
         elif platform=="darwin":
@@ -26,25 +26,23 @@ def build_folders(path_in):
         else:
             raise ValueError('The database folder is not constructed')
 
-
 def construct_nec_folders(project_path):
-     """Check if specific file paths exist (necessary for input and output datasets). If they do 
-     not exist call the function build_folders() and create the folder.
+    """Check if an exisiting file path exists. If not create the necessry folder such that the 
+    file path existis.
 
     Args:
     --------
-    project_path (str): Project root path
+    path_in (str): Project root path
 
     Returns:
     --------
-    Creates the respective folders if they are not yet existent
-
+    Generates the relevant file path
     """
-
+    
     ## Define the file paths
     path_input_raw_data_1 = project_path + 'src/original_data/academic_TRACE'
     path_input_raw_data_2 = project_path + 'src/original_data/academic_TRACE/TRACE_raw'
-    path_input_raw_data_2 = project_path + 'src/original_data/Mergent_FISD'
+    path_input_raw_data_3 = project_path + 'src/original_data/Mergent_FISD'
 
     path_output_clean_data_1 = project_path + 'bld/data/TRACE/'
     path_output_clean_data_2 = project_path + 'bld/data/TRACE/TRACE_raw_clean'
@@ -52,5 +50,6 @@ def construct_nec_folders(project_path):
     # Construct the relevant folders if they are not already exisiting
     build_folders(path_input_raw_data_1)
     build_folders(path_input_raw_data_2)
+    build_folders(path_input_raw_data_3)
     build_folders(path_output_clean_data_1)
     build_folders(path_output_clean_data_2)
